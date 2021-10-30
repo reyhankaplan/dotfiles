b38bf980b217fd684af2f3643fc3851d8dbab2be
Reyhan Unal <rreyhanunal@gmail.com>
2021-10-30 22:50:09 +0300

    fix(nvim): nvimtree config

--- .config/nvim/plugin/packages/nvim_tree.lua
+++ .config/nvim/plugin/packages/nvim_tree.lua
@@ -1,18 +1,19 @@
 local g = vim.g
 local keymap = require('reyhan.utils').keymap
 
-local nvim_tree = require('nvim-tree')
-local tree_cb = require'nvim-tree.config'.nvim_tree_callback
-
 g.nvim_tree_git_hl = 1
 g.nvim_tree_show_icons = {
   git = 0,
+  folders = 1,
+  files = 1,
   folder_arrows = 0,
 }
 
+local nvim_tree = require('nvim-tree')
+local tree_cb = require'nvim-tree.config'.nvim_tree_callback
+
 nvim_tree.setup({
   hijack_cursor = true,
-
   view = {
     mappings = {
       list = {
