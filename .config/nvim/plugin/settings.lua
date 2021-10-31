7a066f52d911fa99387ec9b52f6fe757f14569a6
Reyhan Unal <rreyhanunal@gmail.com>
2021-10-31 16:21:11 +0300

    feat(nvim): add gitsigns plugin

--- .config/nvim/plugin/settings.lua
+++ .config/nvim/plugin/settings.lua
@@ -14,6 +14,7 @@ opt.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
 opt.shiftwidth = 0 -- Number of spaces to use for each step of auto indent
 opt.softtabstop = -1 -- Number of spaces that a <Tab> counts
 opt.expandtab = true -- Use spaces instead of tab characters
+opt.signcolumn = 'yes' -- Always draw the sign column even if there is no sign in it
 
 opt.list = true
 opt.listchars = {
