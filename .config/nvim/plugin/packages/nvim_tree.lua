285a0e2cb7e78bff220c7e7f254c4a6307432e74
Reyhan Unal <rreyhanunal@gmail.com>
2021-10-31 15:29:45 +0300

    feat(nvim): add nvim tree mappings

--- .config/nvim/plugin/packages/nvim_tree.lua
+++ .config/nvim/plugin/packages/nvim_tree.lua
@@ -37,8 +37,29 @@ nvim_tree.setup({
     auto_resize = true,
     mappings = {
       list = {
-        { key = 'l', cb = tree_cb("edit") },
-        { key = 'h', cb = tree_cb("close_node") }
+				{ key = { 'l', '<CR>', '<2-LeftMouse>' }, cb = tree_cb('edit') },
+        { key = 'L', cb = tree_cb('cd') },
+        { key = '<C-s>', cb = tree_cb('split') },
+        { key = '<C-v>', cb = tree_cb('vsplit') },
+        { key = '<C-t>', cb = tree_cb('tabnew') },
+        { key = 'h', cb = tree_cb('close_node') },
+        { key = 'i', cb = tree_cb('preview') },
+        { key = 'R', cb = tree_cb('refresh') },
+        { key = 'c', cb = tree_cb('create') },
+        { key = 'D', cb = tree_cb('remove') },
+        { key = 'r', cb = tree_cb('rename') },
+        { key = 'd', cb = tree_cb('cut') },
+        { key = 'y', cb = tree_cb('copy') },
+        { key = 'p', cb = tree_cb('paste') },
+        { key = 'gyn', cb = tree_cb('copy_name') },
+        { key = 'gyp', cb = tree_cb('copy_path') },
+        {
+          key = 'gya',
+          cb = tree_cb('copy_absolute_path'),
+        },
+        { key = 'H', cb = tree_cb('dir_up') },
+        { key = 's', cb = tree_cb('system_open') },
+        { key = 'q', cb = tree_cb('close') },
       }
     }
   }
